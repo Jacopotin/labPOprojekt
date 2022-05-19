203de05b0e800d6b12e7ae7a97b8afa517441705
Your Name <you@example.com>
2022-05-19 22:33:53 +0200

    created html templates

---
+++ cars/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from .views import *
+
+
+app_name='cars'
+
+urlpatterns=[
+    path('brand', CarView.as_view(), name='type-list'),
+    path('driver', KierowcaView.as_view(), name='type-list'),
+
+]
--- cars/views.py
+++ cars/views.py
@@ -6,14 +6,14 @@ from .models import *
 
 class CarView(generic.ListView):
     model = Samochod
-
+    template_name = "car_list.html"
     def get_queryset(self):
         return Samochod.objects.all()
 
 
 class KierowcaView(generic.ListView):
     model = Kierowca
-
+    template_name = "driver_list.html"
     def get_queryset(self):
         return Kierowca.objects.all()
 
